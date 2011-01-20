3abcab65f801c6125ff15c77d9cab596b1510d02
Bryan Fink <bryan@basho.com>
2011-01-20 16:19:19 -0500

    license and README

--- src/baseball.erl
+++ src/baseball.erl
@@ -1,3 +1,27 @@
+%% -------------------------------------------------------------------
+%% baseball: utilities for computing baseball stats on Riak
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%  http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc Compute baseball stats using Riak Map/Reduce.
+%%
+%%      This module works with data from the Retrosheet archive
+%%      (http://www.retrosheet.org/) to compute baseball statistics
+%%      (currently only batting average).
 -module(baseball).
 
 -export([
